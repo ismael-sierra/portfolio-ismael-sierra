bbc6e1919ba76832f247d619050ed3e589f9452a
ismael-sierra <ismaelsierravega2003@gmail.com>
2026-06-12 20:05:47 +0200

    Noveno commit: Mejora del Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,26 @@ function Home() {
         ¡Hola! Soy <span className="highlight">Ismael Sierra Vega</span>
       </h1>
       <p className="home-description">
-        Desarrollador Full Stack con pasión por la tecnología, la innovación y
-        el aprendizaje continuo. Bienvenido a mi portfolio.
+        Técnico Superior en Desarrollo de Aplicaciones Web, Desarrollo de
+        Aplicaciones Multiplataforma y desarrollador Full Stack. Especializado
+        en gran variedad de tecnologías, con experiencia en el desarrollo de
+        aplicaciones web y móviles.
       </p>
+      <section className="stats-section">
+        {" "}
+        <div className="stat-card">
+          {" "}
+          <h3>2</h3> <p>Proyectos principales</p>{" "}
+        </div>{" "}
+        <div className="stat-card">
+          {" "}
+          <h3>10+</h3> <p>Tecnologías</p>{" "}
+        </div>{" "}
+        <div className="stat-card">
+          {" "}
+          <h3>3</h3> <p>Titulaciones</p>{" "}
+        </div>{" "}
+      </section>
       <a href="/projects" className="home-button">
         Ver proyectos
       </a>
